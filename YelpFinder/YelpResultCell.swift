64e2009920f0d7affcb6f48ed8572566299d5c33
Juan Fernandez <j.c.fernandez@me.com>
2016-04-03 19:26:27 -0700

    Add result cells class. Modify storyboard

---
+++ YelpFinder/YelpResultCell.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class YelpResultCell: UITableViewCell {
+
+    @IBOutlet weak var restaurantImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var starContainer: UIView!
+    @IBOutlet weak var reviews: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var restaurantType: UILabel!
+}

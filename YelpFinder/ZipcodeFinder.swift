73f8c84b5b65e8ffd7e7b80067c52bb33c76238c
Juan Fernandez <j.c.fernandez@me.com>
2016-04-04 02:39:01 -0700

    Zip code finder delegate adjustments.

--- YelpFinder/ZipcodeFinder.swift
+++ YelpFinder/ZipcodeFinder.swift
@@ -3,23 +3,41 @@ import UIKit
 
 typealias ZipcodeFinderCompletion = String? -> ()
 
+protocol ZipcodeFinderDelegate {
+    func zipcodeFinderDidFinish(zipcodeFinder: ZipcodeFinder)
+}
+
 class ZipcodeFinder: NSObject {
     
     let locationManager = CLLocationManager()
-    private let completionBlock : ZipcodeFinderCompletion
+    private (set) var zipcode : String?
+    private let completionBlock : ZipcodeFinderCompletion?
+    private let delegate : ZipcodeFinderDelegate?
     
-    init (completionBlock block: ZipcodeFinderCompletion) {
-        completionBlock = block
+    init (delegate: ZipcodeFinderDelegate?, completionBlock block: ZipcodeFinderCompletion?) {
+        self.completionBlock = block
+        self.delegate = delegate
         super.init()
         self.checkAuthorizationStatus()
     }
     
     func findZipcodeForLocation(location: CLLocation) {
-        let geocoder = CLGeocoder()
         let completion : CLGeocodeCompletionHandler = { placemarks, error in
-            
+            guard let placemarks = placemarks else {
+                self.notify()
+                return
+            }
+            placemarkLoop: for placemark in placemarks {
+                switch placemark.postalCode {
+                case let .Some(postalCode) where !postalCode.isEmpty:
+                    self.notify(withZipcode: postalCode)
+                    break placemarkLoop
+                default:
+                    break
+                }
+            }
         }
-        geocoder.reverseGeocodeLocation(location, completionHandler: completion)
+        CLGeocoder().reverseGeocodeLocation(location, completionHandler: completion)
     }
     
     func checkAuthorizationStatus () {
@@ -28,14 +46,18 @@ class ZipcodeFinder: NSObject {
         switch CLLocationManager.authorizationStatus() {
         case .NotDetermined:
             self.locationManager.requestWhenInUseAuthorization()
-        case .AuthorizedAlways:
-            fallthrough
-        case .AuthorizedWhenInUse:
+        case .AuthorizedAlways, .AuthorizedWhenInUse:
             self.locationManager.requestLocation()
         default:
-            self.completionBlock(nil)
+            self.notify()
         }
     }
+    
+    func notify(withZipcode zipcode: String? = nil) {
+        self.zipcode = zipcode
+        self.completionBlock?(zipcode)
+    }
+    
 }
 
 //MARK: CLLocationManagerDelegate
@@ -45,12 +67,24 @@ extension ZipcodeFinder : CLLocationManagerDelegate {
         if let location = locations.first {
             self.findZipcodeForLocation(location)
         } else {
-            self.completionBlock(nil)
+            self.notify()
         }
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        self.completionBlock(nil)
+        self.notify()
     }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status {
+        case .AuthorizedWhenInUse, .AuthorizedAlways:
+            self.locationManager.requestLocation()
+        case .Restricted, .Denied:
+            self.notify()
+        default:
+            break
+        }
+    }
+    
 }
 

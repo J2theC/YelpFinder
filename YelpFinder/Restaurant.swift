a78b39bd440294805bbc7e5f82a8cb4ee7a317a9
Juan Fernandez <j.c.fernandez@me.com>
2016-04-04 00:58:21 -0700

    shell restaurant model object.

---
+++ YelpFinder/Restaurant.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+class Restaurant {
+    
+    let name : String
+    let reviewsDescription : String
+    let category : String
+    let address : String
+    let distanceDescription : String
+    let priceDescription : String
+    
+    init() {
+        fatalError()
+    }
+}

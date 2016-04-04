d88ba4b4d5626523761adcb9b6941cc56580c00f
Juan Fernandez <j.c.fernandez@me.com>
2016-04-04 00:58:08 -0700

    delegate protocol declaration. delegate property declaration. cell registration

--- YelpFinder/YelpResultsViewController.swift
+++ YelpFinder/YelpResultsViewController.swift
@@ -2,7 +2,17 @@
 import Foundation
 import UIKit
 
+protocol YelpResultsViewControllerDelegate : class {
+    
+}
+
 class YelpResultsViewController : UITableViewController {
+    
+    var delegate : YelpResultsViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        self.tableView.registerClass(YelpResultCell.self, forCellReuseIdentifier: YelpResultCell.reuseIdentifier)
+    }
     
 }
 

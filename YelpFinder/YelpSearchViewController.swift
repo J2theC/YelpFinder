d27b5b421f1d47f55ade89d8030d98c27308d5f8
Juan Fernandez <j.c.fernandez@me.com>
2016-04-04 01:01:15 -0700

    delegate property population. storyboard outlets. protocol conformance.

--- YelpFinder/YelpSearchViewController.swift
+++ YelpFinder/YelpSearchViewController.swift
@@ -1,7 +1,30 @@
-
-import Foundation
 import UIKit
 
-class YelpSearchViewController : UIViewController {
+class YelpSearchViewController : UIViewController  {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var filter: UIBarButtonItem!
+    @IBOutlet weak var search: UIBarButtonItem!
+    private let resultsSegueIdentifier = "resultsSegue"
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        switch (segue.identifier, segue.destinationViewController as? YelpResultsViewController) {
+        case (let .Some(identifier), let .Some(destination))where identifier == self.resultsSegueIdentifier:
+            destination.delegate = self
+            break
+        default:
+            break
+        }
+    }
+    
+}
+
+// MARK: UISearchBarDelegate
+extension YelpSearchViewController : UISearchBarDelegate {
+    
+}
+
+// MARK: YelpResultsViewControllerDelegate
+extension YelpSearchViewController : YelpResultsViewControllerDelegate {
     
 }

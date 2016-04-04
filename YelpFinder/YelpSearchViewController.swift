19fbafd1dfbd4a863900d9ff0cd0b4983d24b8f4
Juan Fernandez <j.c.fernandez@me.com>
2016-04-04 02:39:19 -0700

    Find first zip code on search

--- YelpFinder/YelpSearchViewController.swift
+++ YelpFinder/YelpSearchViewController.swift
@@ -5,7 +5,23 @@ class YelpSearchViewController : UIViewController  {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var filter: UIBarButtonItem!
     @IBOutlet weak var search: UIBarButtonItem!
+    private var zipcodeFinder : ZipcodeFinder?
     private let resultsSegueIdentifier = "resultsSegue"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.loadFirstZipcode()
+    }
+    
+    func loadFirstZipcode() {
+        let firstZipcode : ZipcodeFinderCompletion = { zipcode in
+            if let zipcode = zipcode where !zipcode.isEmpty {
+                self.searchBar.text = zipcode
+                self.showLoadingIndicatorForSearchParamater(zipcode)
+            }
+        }
+        self.zipcodeFinder = ZipcodeFinder(delegate: self, completionBlock: firstZipcode)
+    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         switch (segue.identifier, segue.destinationViewController as? YelpResultsViewController) {
@@ -15,6 +31,10 @@ class YelpSearchViewController : UIViewController  {
         default:
             break
         }
+    }
+    
+    func showLoadingIndicatorForSearchParamater(searchParamater: String) {
+        
     }
     
 }
@@ -27,4 +47,13 @@ extension YelpSearchViewController : UISearchBarDelegate {
 // MARK: YelpResultsViewControllerDelegate
 extension YelpSearchViewController : YelpResultsViewControllerDelegate {
     
+}
+
+extension YelpSearchViewController : ZipcodeFinderDelegate {
+    
+    func zipcodeFinderDidFinish(zipcodeFinder: ZipcodeFinder) {
+        if zipcodeFinder ==  self.zipcodeFinder {
+            self.zipcodeFinder = nil
+        }
+    }
 }
